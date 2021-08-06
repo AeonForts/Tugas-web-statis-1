bb41c2f653b647649408d7ab88bd01cc63fc58c5
AeonForts <88488324+AeonForts@users.noreply.github.com>
2021-08-06 07:48:57 +0700

    Add files via upload

---
+++ Gallery.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Tugas 2 tentang Web statis</title>
+    </head>
+        <body>
+          <table border="1" width="800px" height="1000" align="center">
+
+            <!--Header -->
+                <tr height="200px">
+                 <td>
+                    <image src="Girls.Frontline.full.2032918.jpg" width="100%" height="100%">
+                 </td>
+                </tr>
+
+            <!--Menu-->
+                <tr height="100px">
+                 <td align="center">
+                <!--Internal link-->
+                    <a href="index.php"><strong>Beranda</strong></a>
+                    <a href="Profile.php"><strong>Profile</strong></a>
+                    <a href="Gallery.php"><strong>Gallery</strong></a>
+                    <a href="Video.php"><strong>Video</strong></a>
+                    <a href="Contact.php"><strong>Contact</strong></a>
+                <!--Link menuju website -->
+                    <a href="https://www.facebook.com/"><strong>Facebook</strong></a>
+                    <a href="https://www.instagram.com/"><strong>Instragram</strong></a>
+                 
+                 </td>
+                </tr>
+
+            <!-- Content-->
+                <tr height="600">
+                    <td valign="left">
+                        <image src="Al_Khwarizmi2.jpg" width="200px">
+                    </td>
+                </tr>
+
+            <!-- Footer-->
+                <tr height="100">
+                 <td>XI RPL 1</td>
+                </tr>
+                
+        </body>     
+</html>
---
+++ Video.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Tugas 2 tentang Web statis</title>
+    </head>
+        <body>
+          <table border="1" width="800px" height="1000" align="center">
+
+            <!--Header -->
+                <tr height="200px">
+                 <td>
+                    <image src="Girls.Frontline.full.2032918.jpg" width="100%" height="100%">
+                 </td>
+                </tr>
+
+            <!--Menu-->
+                <tr height="100px">
+                 <td align="center">
+                <!--Internal link-->
+                    <a href="index.php"><strong>Beranda</strong></a>
+                    <a href="Profile.php"><strong>Profile</strong></a>
+                    <a href="Gallery.php"><strong>Gallery</strong></a>
+                    <a href="Video.php"><strong>Video</strong></a>
+                    <a href="Contact.php"><strong>Contact</strong></a>
+                <!--Link menuju website -->
+                    <a href="https://www.facebook.com/"><strong>Facebook</strong></a>
+                    <a href="https://www.instagram.com/"><strong>Instragram</strong></a>
+                 
+                 </td>
+                </tr>
+
+            <!-- Content-->
+                <tr height="600">
+                    <td valign="top">
+                    
+                         <table>
+                            <tr>
+                                <td>
+                                    <embed autostart="false" height="100%" src="https://youtu.be/d01jUnduKNQ" width="250px"></embed>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+            <!-- Footer-->
+                <tr height="100">
+                 <td>XI RPL 1</td>
+                </tr>
+                
+        </body>     
+</html>
